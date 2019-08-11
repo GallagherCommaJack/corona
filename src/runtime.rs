411655db9c03b2c77260e6eb8217e4a41e7a9a29
theseriousadult <jack@gallabytes.com>
2019-08-11 15:42:39 -0700

    fix+pass tests, remove dependency on `tokio-io-pool`
    Turns out rolling our own threadpool was simpler and has fewer gotcha's.

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,10 +1,28 @@
 use crate::prelude::*;
+use futures::future::Future;
 use std::any::Any;
+use std::cell::RefCell;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
+use std::thread;
 use tokio::sync::mpsc::{unbounded_channel as channel, UnboundedSender as Sender};
 use tokio::sync::oneshot;
-use tokio_io_pool as tip;
+
+macro_rules! womp {
+    () => {
+        &format!("{}:{}:{}", file!(), line!(), column!())
+    };
+    ($message:expr) => {
+        &format!("{}:{}:{} {}", file!(), line!(), column!(), $message)
+    };
+}
+
+thread_local! {
+    static POOL: RefCell<Arc<RoundRobin>> = RefCell::new(Arc::new(RoundRobin {
+        current: AtomicUsize::new(0),
+        senders: Vec::new()
+    }));
+}
 
 type SendableTask = Box<FnOnce() -> Box<Any + Send + 'static> + Send + 'static>;
 
@@ -17,21 +35,21 @@ pub struct RoundRobin {
 
 impl RoundRobin {
     pub fn next_thread(&self) -> usize {
-        self.current.fetch_add(1, Ordering::Relaxed)
+        self.current.fetch_add(1, Ordering::SeqCst)
     }
 
     pub fn next_sender(&self) -> Sender<SendableReq> {
         let id = self.next_thread();
         let len = self.senders.len();
         let ix = id % len;
-        self.senders
-            .get(ix)
-            .unwrap_or_else(|| panic!("{}", line!()))
-            .clone()
+        self.senders.get(ix).expect(womp!()).clone()
     }
 
     // TODO: better error type here
-    pub fn spawn<R: Send + 'static, F: FnOnce() -> R + Send + 'static>(&self, f: F) -> R {
+    pub fn spawn<R: Send + 'static, F: FnOnce() -> R + Send + 'static>(
+        &self,
+        f: F,
+    ) -> impl Future<Item = R, Error = oneshot::error::RecvError> {
         let (sender, receiver) = oneshot::channel();
         let mut handle = self.next_sender();
         let task: SendableTask = Box::new(move || {
@@ -40,15 +58,13 @@ impl RoundRobin {
             boxed
         });
         handle
-            .coro_send((sender, task))
-            .unwrap_or_else(|e| panic!("{}: {:?}", line!(), e));
-        let any = receiver
-            .coro_wait()
-            .unwrap_or_else(|e| panic!("{}: {:?}", line!(), e));
-        let res: Box<R> = any
-            .downcast()
-            .unwrap_or_else(|e| panic!("{}: {:?}", line!(), e));
-        *res
+            .try_send((sender, task))
+            .unwrap_or_else(|_| panic!("failed to send - did you drop the runtime?",));
+        receiver.map(|any| *any.downcast().expect(womp!()))
+    }
+
+    pub fn with_thread_lcoal() -> Arc<Self> {
+        POOL.with(|p| p.borrow().clone())
     }
 }
 
@@ -133,65 +149,62 @@ impl Builder {
             current: AtomicUsize::new(0),
             senders,
         });
-        let pool = tip::Builder::default()
-            .pool_size(pool_size)
-            .name_prefix(name_prefix)
-            .after_start(move || after_start())
-            .before_stop(move || before_stop())
-            .build()
-            .unwrap_or_else(|e| panic!("{}: {}", line!(), e));
+        let mut pool = Vec::with_capacity(pool_size);
         for _ in 0..pool_size {
             let coro = coro.clone();
             let inner_spawner = spawner.clone();
             let receivers = receivers.clone();
-            pool.spawn(futures::future::lazy(move || {
-                coro.set_thread_local()
-                    .unwrap_or_else(|e| panic!("{}: {}", line!(), e));
-                let id = inner_spawner.next_thread();
-                let tasks = {
-                    let mut lock = receivers.lock();
-                    lock.get_mut(id)
-                        .unwrap_or_else(|| panic!("{}", line!()))
-                        .take()
-                        .unwrap_or_else(|| panic!("{}", line!()))
-                };
-                let c2 = coro.clone();
-                coro.spawn(move || {
-                    for (o, t) in tasks.iter_ok() {
-                        o.send(t())
-                            .unwrap_or_else(|e| panic!("{}: {:?}", line!(), e));
-                    }
-                })
-                .unwrap_or_else(|e| panic!("{}: {}", line!(), e));
-                futures::future::ok::<(), ()>(())
-            }))
-            .unwrap_or_else(|e| panic!("{}: {}", line!(), e));
+            let after_start = after_start.clone();
+            let before_stop = before_stop.clone();
+            let id = inner_spawner.next_thread();
+            dbg!(id);
+            let handle = thread::Builder::new()
+                .name(format!("{}_{}", name_prefix, id))
+                .spawn(move || {
+                    coro.set_thread_local().expect(womp!());
+
+                    let sp2 = inner_spawner.clone();
+                    POOL.with(move |o| o.replace(sp2));
+
+                    let tasks = {
+                        let mut lock = receivers.lock();
+                        lock.get_mut(id).expect(womp!()).take().expect(womp!())
+                    };
+                    coro.run(move || {
+                        after_start();
+                        for (o, t) in tasks.iter_ok() {
+                            let res = t();
+                            o.send(res).expect(womp!())
+                        }
+                        before_stop();
+                    })
+                })?;
+            pool.push(handle);
         }
-        // .after_start(move || {
-        //     after_start();
-        // })
         Ok(Runtime { pool, spawner })
     }
 }
 
 pub struct Runtime {
-    pub pool: tip::Runtime,
+    pub pool: Vec<thread::JoinHandle<Result<(), crate::errors::StackError>>>,
     pub spawner: Arc<RoundRobin>,
 }
 
-impl Runtime {}
-
 #[derive(Clone)]
 pub struct Handle {
-    pub inner: tip::Handle,
+    // pub inner: Vec<thread::JoinHandle>,
     pub spawner: Arc<RoundRobin>,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use futures::future::Future;
     use std::sync::atomic::{AtomicBool, Ordering};
     use std::sync::Arc;
+    use std::time::Duration;
+    use tokio::clock;
+    use tokio::timer::Delay;
 
     #[test]
     fn spawn_some() {
@@ -206,29 +219,52 @@ mod tests {
         let runtime = Builder::default()
             .coro(coro.clone())
             .build()
-            .unwrap_or_else(|e| panic!("{}: {}", line!(), e));
+            .expect(womp!());
 
         let c2 = coro.clone();
-        let result = coro.run(move || {
-            let c = c2.clone();
-            runtime.spawner.spawn(move || {
-                let result = c
+        let result = coro
+            .run(move || {
+                let c = c2.clone();
+                runtime
+                    .spawner
                     .spawn(move || {
-                        s2c.store(true, Ordering::Relaxed);
-                        42usize
+                        let result = c
+                            .spawn(move || {
+                                s2c.store(true, Ordering::Relaxed);
+                                42usize
+                            })
+                            .expect(womp!())
+                            .coro_wait()
+                            .expect(womp!());
+                        s1c.store(true, Ordering::Relaxed);
+                        result
                     })
-                    .unwrap_or_else(|e| panic!("{}: {}", line!(), e))
                     .coro_wait()
-                    .unwrap_or_else(|e| panic!("{}: {}", line!(), e));
-                s1c.store(true, Ordering::Relaxed);
-                result
             })
-        });
+            .expect(womp!());
 
         // Both coroutines run to finish
         assert!(s1.load(Ordering::Relaxed), "The outer closure didn't run");
         assert!(s2.load(Ordering::Relaxed), "The inner closure didn't run");
         // The result gets propagated through.
-        assert_eq!(42, result.unwrap_or_else(|e| panic!("{}: {}", line!(), e)));
+        assert_eq!(42, result.expect(womp!()));
+    }
+
+    /// Wait for a future to complete.
+    #[test]
+    fn future_wait() {
+        let runtime = Builder::default().pool_size(4).build().expect(womp!());
+        let (sender, receiver) = oneshot::channel();
+        let all_done = runtime
+            .spawner
+            .spawn(move || receiver.coro_wait().expect(womp!()));
+        let delayed = runtime.spawner.spawn(move || {
+            let timeout = Delay::new(clock::now() + Duration::from_millis(50));
+            timeout.coro_wait().expect(womp!());
+            sender.send(42).expect(womp!());
+        });
+        let res = Future::wait(delayed.join(all_done));
+        // drop(runtime);
+        assert_eq!(42, res.expect(womp!()).1);
     }
 }
